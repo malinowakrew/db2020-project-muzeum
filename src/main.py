24d5b46c4bba595f6f8c83b4d55dd78cbca4d664
malinowakrew <51093851+malinowakrew@users.noreply.github.com>
2020-05-18 19:59:40 +0200

    niezalogowany

--- src/main.py
+++ src/main.py
@@ -1,9 +1,44 @@
 import sql.eksponaty as api
+import os
+import sql.wystawy as wystawy
+import datetime
+
 
 print("Witamy w naszym muzeum")
-print("Wyszukaj kolumny - tymczasowe")
-co = input("kim jesteś?")
-if(co == "kierownik"):
-    api.kolumny()
-else:
-    api.wyszukiwarka()
+print("Zaloguj się na swoje konto aby mieć dostęp do większej ilości funkcjonalności")
+zmienna = 1
+
+
+while(zmienna):
+    print("Wybierz co chcesz zrobić z naszego menu: \n"
+          "1. Zaloguj się \n"
+          "2. Załóż konto \n"
+          "3. Przeglądaj zbiory bez konta \n")
+    funkcjonalnosc = input("Podaj numer, który Cię interesuje: ")
+
+    if (funkcjonalnosc == "1"):
+        # clear = lambda: os.system('cls')
+        # clear()
+        print("Witamy u nas")
+    elif (funkcjonalnosc == "2"):
+        pass
+    elif (funkcjonalnosc == "3"):
+        print("\n \t ################### \n"
+              "Witamy, możesz w ograniczonu sposób korzystać z naszej bazy. \n"
+              "1. Sprawdź aktualne wystawy \n"
+              "2. Sprawdź popularne wystawy \n")
+        funkcjonalnosc = input("Podaj numer, który Cię interesuje: ")
+        niezalogowany = wystawy.niezalogowany()
+
+        if (funkcjonalnosc == "1"):
+            dzis = datetime.datetime.now()
+            niezalogowany.wyszukiwarka_aktywnych_wystaw()
+            zmienna =  0
+        elif (funkcjonalnosc == "2"):
+            niezalogowany.najczesciej_odwiedzane_wystawy()
+        print("\n \t ################### \n")
+
+    else:
+        print("Mamy błąd - źle wybrałeś spróbuj ponownie.")
+        zmienna = 1
+
--- src/sql/eksponaty.py
+++ src/sql/eksponaty.py
@@ -1,57 +1,77 @@
 import os
 import pymysql
+import datetime
 
-def kolumny():
-    connection = pymysql.connect(
-        host='localhost',
-        # user=os.getenv("DB_USERNAME"),
-        user="admin",
-        # password=os.getenv("DB_PASSWORD"),
-        password="123",
-        database="muzeum",
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
+class niezalogowany():
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host='localhost',
+            # user=os.getenv("DB_USERNAME"),
+            user="admin",
+            # password=os.getenv("DB_PASSWORD"),
+            password="123",
+            database="muzeum",
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
 
-    try:
-        with connection.cursor() as cursor:
-            tabela = input("co chcesz zmodyfikować?")
-            sql = f"show columns from {tabela}"
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            kolumny = []
-            for item in result:
-                print(item['Field'])
-                kolumny.append(item['Field'])
-        return 1
-    except:
-        print("Przepraszamy wystąpił błąd")
-        return 0
-    finally:
-        connection.close()
+    def wyszukiwarka_aktywnych_wystaw(self):
+        try:
+            with self.connection.cursor() as cursor:
+                # sprawdzamy dzisiejszą datę
+                dzisiejsza_data = datetime.datetime.now()
+                dzis = dzisiejsza_data.strftime("%Y-%m-%d")
+                print(f"\t Dziś mamy {dzis}")
 
+                # wykonujemy zapytanie do bazy i wyświetlamy
+                sql = f"SELECT nazwa, koniec FROM wystawa WHERE koniec > DATE '{dzis}' AND poczatek < DATE '{dzis}'"
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                for iter, wystawa in enumerate(result):
+                    print(f"{iter+1}. {wystawa['nazwa']} \t otwarta do {wystawa['koniec']}")
 
-def wyszukiwarka():
-    connection = pymysql.connect(
-        host='localhost',
-        # user=os.getenv("DB_USERNAME"),
-        user="admin",
-        # password=os.getenv("DB_PASSWORD"),
-        password="123",
-        database="muzeum",
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
+                # informacje dodatkowe dla użytkownika
+                if(dzisiejsza_data.weekday() == 0):
+                    print("Pamiętaj tylko, że dziś poniedziałek! MUZEUM NIECZYNNE :)")
 
-    try:
-        with connection.cursor() as cursor:
-            nazwa_stylu = input("Podaj nazwę stylu")
-            opis = input("Podaj opis stylu")
-            lista = [nazwa_stylu, opis]
-            kolumny = "nazwa_stylu, opis"
-            sql = "INSERT INTO styl (nazwa_stylu, opis) VALUES ('nic', 'nic')"
-            cursor.execute(sql)
-            connection.commit()
+            return 1
+        except:
+            print("Przepraszamy wystąpił błąd jakiś tam dziki")
+            return 0
+        finally:
+            self.connection.close()
 
-    finally:
-        connection.close()
+    def najczesciej_odwiedzane_wystawy(self):
+        try:
+            with self.connection.cursor() as cursor:
+                # wybieramy opcje wyszukiwania
+                wybor = input("Czy chcesz oglądać tylko aktywne wystawy? tak/nie ")
+                wybor = wybor.lower()
+
+                # konstruujemy odpowiednie zapytania
+                if(wybor == "nie"):
+                    sql = "SELECT wystawa.nazwa, wystawa.koniec, wystawa.poczatek, COUNT(bilet.biletID) AS ilosc"\
+                            "FROM wystawa JOIN bilet ON wystawa.wystawaID = bilet.wystawaID"\
+                            "WHERE wystawa.koniec > DATE '2030-01-01'"\
+                            "GROUP BY wystawa.wystawaID ORDER BY ilosc"
+                    sql2 = "SELECT wystawa.nazwa, wystawa.koniec, wystawa.poczatek, COUNT(bilet.biletID) AS ilosc FROM wystawa JOIN bilet ON wystawa.wystawaID = bilet.wystawaID GROUP BY wystawa.wystawaID ORDER BY ilosc DESC LIMIT 5"
+                    print("Aktywne TOP 5")
+                elif(wybor == "tak"):
+                    dzisiejsza_data = datetime.datetime.now()
+                    dzis = dzisiejsza_data.strftime("%Y-%m-%d")
+                    sql2 = f"SELECT wystawa.nazwa, wystawa.koniec, wystawa.poczatek, COUNT(bilet.biletID) AS ilosc FROM wystawa JOIN bilet ON wystawa.wystawaID = bilet.wystawaID WHERE DATE '{dzis}' BETWEEN wystawa.poczatek AND wystawa.koniec GROUP BY wystawa.wystawaID ORDER BY ilosc DESC LIMIT 5"
+
+                    print("TOP 5")
+                else:
+                    raise EOFError
+                cursor.execute(sql2)
+                result = cursor.fetchall()
+                for iter, wystawa in enumerate(result):
+                    print(f"{iter + 1}. {wystawa['nazwa']} \t otwarta do {wystawa['koniec']}")
+                # wykonujemy zapytanie
+            return 1
+        except:
+            print("Przepraszamy wystąpił błąd jakiś tam dziki")
+            return 0
+        finally:
+            self.connection.close()
